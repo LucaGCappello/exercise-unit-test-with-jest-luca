da7ec0410eb5fee2c9af757be887b0714745f1d7
LucaGCappello <152432062+LucaGCappello@users.noreply.github.com>
2024-03-22 18:53:46 +0000

    jest testings

---
+++ app.js
@@ -0,0 +1,17 @@
+// We declare the function with the exact name "fromEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.07;
+    // return the dollar value
+    return valueInDollar;
+}
+
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
+const sum = (a,b) => {
+    return a + b
+}
+module.exports = { sum, fromEuroToDollar }
